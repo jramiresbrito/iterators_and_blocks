42d18427928c2e3875c724165c2b1f2f282bf6b4
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2020-10-22 13:12:49 -0300

    ADD README

---
+++ README.md
@@ -0,0 +1,3 @@
+# CODE FOR ITERATORS AND BLOCKS CLASS - LE WAGON BOOTCAMP BATCH 513
+
+## Leiam com atenção os comentários explicativos que adicionei em cada arquivo.
--- blocks.rb
+++
@@ -1,8 +0,0 @@
-musicians = ['David Gilmour', 'Roger Waters', 'Richard Wright', 'Nick Mason']
-
-upcased_first_names = musicians.map do |musician|
-  first_name = musician.split.first
-  upcased_first_name = first_name.upcase
-end
-
-p upcased_first_names
